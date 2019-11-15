a206bab8b9262b84934122f17eb887a9d72f0d83
Jamie <jamier@o3world.com>
2019-11-15 17:10:46 -0500

    Updates

---
+++ precache-manifest.de188b4df27eb1403bb0d4677525e636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8699f6ac53626ac502a0b7c3b1a9cc",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "12a3d4c753883014265d",
+    "url": "/todolist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "809d8576d85705e653c4",
+    "url": "/todolist/static/js/2.e89a4b25.chunk.js"
+  },
+  {
+    "revision": "12a3d4c753883014265d",
+    "url": "/todolist/static/js/main.b396c25b.chunk.js"
+  },
+  {
+    "revision": "773db8ed56ba2d1300d5",
+    "url": "/todolist/static/js/runtime-main.5baa8871.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/todolist/static/media/logo.25bf045c.svg"
+  }
+]);
