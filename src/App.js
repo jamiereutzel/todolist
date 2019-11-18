6b79ec0c5d0733ba2c3245cfff70c87f80781422
Jamie <jamier@o3world.com>
2019-11-18 16:12:23 -0500

    Build Out Some Basic Styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <>
       <h1>Todo List</h1>
+
+      <div>
+        <input type="text" />
+        <button class="btn ml-1">Add</button>
+        <button class="btn ml-1">Clear Completed</button>
+      </div>
     </>
   )
 }
